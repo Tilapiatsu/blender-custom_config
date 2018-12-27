7db8c5505b6e20e8b4fee0a0545fcd6c16654fd0
Tilapiatsu <tilapiatsu@hotmail.fr>
2018-12-27 12:44:39 +0100

    * update the Addon updater from submodule

--- scripts/addons/UpdateAddonFromGitSubmodule.bat
+++ scripts/addons/UpdateAddonFromGitSubmodule.bat
@@ -1,6 +1,12 @@
 
 set param=/e /r:10 /w:1 /bytes /fp
+
 robocopy "Capsule_GIT\Capsule" "Capsule" %param%
+robocopy "simple-asset-manager_GIT\SimpleAssetManager" "SimpleAssetManager" %param%
+robocopy "fSpy-Blender_GIT\fspy_blender" "fspy_blender" %param%
+robocopy "glTF_Blender-IO_GIT\addons\io_sketchfab_plugin" "glTF_Blender-IO" %param%
+
 copy "RenderBurst_GIT\RenderBurst.py" .
+copy "object-shake\Object_Shake.py" .
 
 pause
