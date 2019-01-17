1315e88dcf5320f2c4ebdc5ec472a9cfd5e2b4c2
Tilapiatsu <tilapiatsu@hotmail.fr>
2019-01-17 08:34:36 +0100

    * update mesh_align_plus to 2.8 branch

--- scripts/addons/UpdateAddonFromGitSubmodule.bat
+++ scripts/addons/UpdateAddonFromGitSubmodule.bat
@@ -5,6 +5,7 @@ robocopy "Capsule_GIT\Capsule" "Capsule" %param%
 robocopy "simple-asset-manager_GIT\SimpleAssetManager" "SimpleAssetManager" %param%
 robocopy "fSpy-Blender_GIT\fspy_blender" "fspy_blender" %param%
 robocopy "glTF_Blender-IO_GIT\addons\io_sketchfab_plugin" "glTF_Blender-IO" %param%
+robocopy "mesh_align_plus_GIT\mesh_mesh_align_plus" "mesh_align_plus" %param%
 
 copy "RenderBurst_GIT\RenderBurst.py" .
 copy "object-shake\Object_Shake.py" .
