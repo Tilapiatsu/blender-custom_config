6dedf1017768404b7253450d183c52501809e1f4
Tilapiatsu <tilapiatsu@hotmail.fr>
2019-02-15 23:03:12 +0100

    * Isolate work in object mode

--- scripts/addons/Tila_Config/tila_OP_Isolate.py
+++ scripts/addons/Tila_Config/tila_OP_Isolate.py
@@ -22,36 +22,37 @@ class TILA_isolate(bpy.types.Operator):
 
     force_object_isolate = bpy.props.BoolProperty(name='force_object_isolate', default=False)
 
-    is_isolated=False
-    isolated_items=[]
+    is_isolated = False
+    isolated_items = []
 
     def modal(self, context, event):
         pass
 
     def isolate(self, isolate=None, reveal=None):
-        if self.active_object is None or self.is_isolated:
+        if len(self.selected_objects) == 0 or self.is_isolated:
             reveal()
+            self.is_isolated = False
+            self.isolated_items = []
         else:
             isolate(unselected=True)
-            self.is_isolated=True
-            self.isolated_items.append(self.active_object)
+            self.is_isolated = True
+            self.isolated_items.append(self.selected_objects)
 
     def invoke(self, context, event):
-        self.active_object = bpy.context.active_object
+        self.selected_objects = bpy.context.selected_objects
         if context.space_data.type == 'VIEW_3D':
-            
+
             if bpy.context.mode == 'OBJECT':
                 self.isolate(isolate=bpy.ops.object.hide_view_set, reveal=bpy.ops.object.hide_view_clear)
-            elif bpy.context.mode in ['EDIT_MESH','EDIT_CURVE', 'EDIT_SURFACE']:
-                pass
+            elif bpy.context.mode in ['EDIT_MESH', 'EDIT_CURVE', 'EDIT_SURFACE']:
+                self.isolate(isolate=bpy.ops.mesh.hide, reveal=bpy.ops.mesh.reveal)
             elif bpy.context.mode in ['PAINT_GPENCIL', 'EDIT_GPENCIL', 'SCULPT_GPENCIL']:
                 pass
 
         if context.space_data.type == 'OUTLINER':
             pass
-        
-        return {'FINISHED'}
 
+        return {'FINISHED'}
 
 
 classes = (
