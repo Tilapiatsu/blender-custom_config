b460f23cbf05c7f36f0d482fcd54293573fa3477
Tilapiatsu <tilapiatsu@hotmail.fr>
2019-02-25 00:49:03 +0100

    * update mesh_align_plus submlodule name

--- scripts/addons/UpdateAddonFromGitSubmodule.bat
+++ scripts/addons/UpdateAddonFromGitSubmodule.bat
@@ -5,7 +5,7 @@ robocopy "..\GIT\Capsule\Capsule" "Capsule" %param%
 robocopy "..\GIT\simple-asset-manager\SimpleAssetManager" "SimpleAssetManager" %param%
 robocopy "..\GIT\fSpy-Blender\fspy_blender" "fspy_blender" %param%
 robocopy "..\GIT\glTF_Blender-IO\addons\io_sketchfab_plugin" "glTF_Blender-IO" %param%
-robocopy "..\GIT\mesh_align_plus\mesh_mesh_align_plus" "mesh_align_plus" %param%
+robocopy "..\GIT\mesh_align_plus\mesh_mesh_align_plus" "mesh_mesh_align_plus" %param%
 
 copy "RenderBurst_GIT\RenderBurst.py" .
 copy "object-shake\Object_Shake.py" .
