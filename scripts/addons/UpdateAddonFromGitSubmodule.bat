2c8dd69d80ebfd4dc324706a87163177897e81af
Tilapiatsu <tilapiatsu@hotmail.fr>
2019-02-25 00:26:36 +0100

    * move submodules to new location

--- scripts/addons/UpdateAddonFromGitSubmodule.bat
+++ scripts/addons/UpdateAddonFromGitSubmodule.bat
@@ -1,11 +1,11 @@
 
 set param=/e /purge /r:10 /w:1 /bytes /fp
 
-robocopy "Capsule_GIT\Capsule" "Capsule" %param%
-robocopy "simple-asset-manager_GIT\SimpleAssetManager" "SimpleAssetManager" %param%
-robocopy "fSpy-Blender_GIT\fspy_blender" "fspy_blender" %param%
-robocopy "glTF_Blender-IO_GIT\addons\io_sketchfab_plugin" "glTF_Blender-IO" %param%
-robocopy "mesh_align_plus_GIT\mesh_mesh_align_plus" "mesh_align_plus" %param%
+robocopy "..\GIT\Capsule\Capsule" "Capsule" %param%
+robocopy "..\GIT\simple-asset-manager\SimpleAssetManager" "SimpleAssetManager" %param%
+robocopy "..\GIT\fSpy-Blender\fspy_blender" "fspy_blender" %param%
+robocopy "..\GIT\glTF_Blender-IO\addons\io_sketchfab_plugin" "glTF_Blender-IO" %param%
+robocopy "..\GIT\mesh_align_plus\mesh_mesh_align_plus" "mesh_align_plus" %param%
 
 copy "RenderBurst_GIT\RenderBurst.py" .
 copy "object-shake\Object_Shake.py" .
