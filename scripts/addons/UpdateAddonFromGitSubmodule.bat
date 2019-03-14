a9960450414985184d39eac39dccf61bd5d8ef81
Tilapiatsu <tilapiatsu@hotmail.fr>
2019-03-14 11:35:35 +0100

    * update gitignore and update script

--- scripts/addons/UpdateAddonFromGitSubmodule.bat
+++ scripts/addons/UpdateAddonFromGitSubmodule.bat
@@ -7,6 +7,7 @@ robocopy "..\GIT\fSpy-Blender\fspy_blender" "fspy_blender" %param%
 robocopy "..\GIT\glTF_Blender-IO\addons\io_sketchfab_plugin" "glTF_Blender-IO" %param%
 robocopy "..\GIT\mesh_align_plus\mesh_mesh_align_plus" "mesh_mesh_align_plus" %param%
 robocopy "..\GIT\glTF-Blender-IO\addons\io_scene_gltf2" "glTF-Blender-IO" %param%
+robocopy "..\GIT\ScreenCast-Keys\src\screencastkeys" "screencastkeys" %param%
 
 copy "RenderBurst_GIT\RenderBurst.py" .
 copy "object-shake\Object_Shake.py" .
