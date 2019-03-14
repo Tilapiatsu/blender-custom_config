c0fe47da14562ac69fbe82f7d8901acdd3ce177c
Tilapiatsu <tilapiatsu@hotmail.fr>
2019-03-14 13:09:38 +0100

    + add TiNA Submodule

--- scripts/addons/UpdateAddonFromGitSubmodule.bat
+++ scripts/addons/UpdateAddonFromGitSubmodule.bat
@@ -9,7 +9,7 @@ robocopy "..\GIT\mesh_align_plus\mesh_mesh_align_plus" "mesh_mesh_align_plus" %p
 robocopy "..\GIT\glTF-Blender-IO\addons\io_scene_gltf2" "glTF-Blender-IO" %param%
 robocopy "..\GIT\ScreenCast-Keys\src\screencastkeys" "screencastkeys" %param%
 
-copy "RenderBurst_GIT\RenderBurst.py" .
 copy "object-shake\Object_Shake.py" .
+copy "..\GIT\TiNA\TiNA.py" .
 
 pause
