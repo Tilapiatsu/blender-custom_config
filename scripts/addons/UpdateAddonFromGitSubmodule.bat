cd3cc07ca8098d89e00b2e158ea965198d8e0187
Tilapiatsu <tilapiatsu@hotmail.fr>
2019-07-07 22:30:39 +0200

    * updateAddonFromGitSumbodule script updated

--- scripts/addons/UpdateAddonFromGitSubmodule.bat
+++ scripts/addons/UpdateAddonFromGitSubmodule.bat
@@ -8,6 +8,7 @@ robocopy "..\GIT\glTF_Blender-IO\addons\io_sketchfab_plugin" "glTF_Blender-IO" %
 robocopy "..\GIT\mesh_align_plus\mesh_mesh_align_plus" "mesh_mesh_align_plus" %param%
 robocopy "..\GIT\glTF-Blender-IO\addons\io_scene_gltf2" "glTF-Blender-IO" %param%
 robocopy "..\GIT\ScreenCast-Keys\src\screencastkeys" "screencastkeys" %param%
+robocopy "..\GIT\mifthtools\blender\addons\2.8\mira_tools" "mira_tools" %param%
 
 copy "..\GIT\RenderBurst\RenderBurst.py" .
 copy "..\GIT\object-shake\Object_Shake.py" .
