250fb324ae90940f605032c267bfe51311b79f74
Tilapiatsu <tilapiatsu@hotmail.fr>
2020-01-21 20:08:27 +0100

    + add leomoon lightstudio

---
+++ scripts/addons/leomoon-lightstudio/__init__.py
@@ -0,0 +1,42 @@
+#Created by Striam Sp. z o.o.
+
+bl_info = {
+    "name": "LeoMoon LightStudio",
+    "description": "Easy setup for complex studio lighting",
+    "author": "LeoMoon Studios",
+    "version": (2, 4, 1),
+    "blender": (2, 80, 0),
+    "location": "View3D -> Tools -> Light Studio",
+    "wiki_url": "",
+    "category": "User Interface" }
+    
+    
+import bpy      
+
+# load and reload submodules
+##################################    
+
+from . import auto_load
+
+auto_load.init()
+
+
+# register
+################################## 
+
+from . light_operators import Blender_Light_Studio_Properties
+from . import deleteOperator
+from . import light_preview_list
+
+def register():
+    auto_load.register()
+
+    bpy.types.Scene.BLStudio = bpy.props.PointerProperty(name="Blender Light Studio Properties", type = Blender_Light_Studio_Properties)
+    bpy.types.Object.protected = bpy.props.BoolProperty(name = 'protected', default = False)
+    deleteOperator.add_shortkeys()
+    light_preview_list.register()
+    
+
+def unregister():
+    deleteOperator.remove_shortkeys()
+    auto_load.unregister()
