ba634a7120bf7a5eb0c0fe8b67bf3b4c08273689
Tilapiatsu <tilapiatsu@hotmail.fr>
2020-02-29 14:19:28 +0100

    + add texel Density checker

--- scripts/addons/UpdateAddonFromGitSubmodule.bat
+++ scripts/addons/UpdateAddonFromGitSubmodule.bat
@@ -29,6 +29,7 @@ copy "..\GIT\bpy_photogrametry\system_time_tracker.py" .
 copy "..\GIT\bpy_photogrametry\uv_tube_unwrap.py" .
 copy "..\GIT\bpy_photogrametry\space_view3d_point_cloud_visualizer.py" .
 copy "..\GIT\advanced_transform\advanced_transform_2_8.py" .
+copy "..\GIT\Texel-Density-Checker\Texel_Density_2_2_1_281.py" .
 robocopy "..\GIT\bpy_photogrametry\io_mesh_fast_obj" "io_mesh_fast_obj" %param%
 
 pause
