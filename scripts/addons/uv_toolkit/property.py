3819464ec19f3e6be6f89cc309129625d58a4a09
Tilapiatsu <tilapiatsu@hotmail.fr>
2020-09-26 22:54:38 +0200

    - remove UV Toolkit

--- scripts/addons/uv_toolkit/property.py
+++
@@ -1,8 +0,0 @@
-import bpy
-
-from bpy.props import IntProperty
-
-
-class UvToolkitProperties(bpy.types.PropertyGroup):
-    checker_map_width: IntProperty(name="Width", min=0)
-    checker_map_height: IntProperty(name="Height", min=0)
