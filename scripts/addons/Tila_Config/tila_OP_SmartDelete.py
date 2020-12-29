c83385db5082c4b1e6778c8a7dd0de5236727f06
Tilapiatsu <tilapiatsu@hotmail.fr>
2020-12-29 23:16:24 +0100

    * update Smart delete

--- scripts/addons/Tila_Config/tila_OP_SmartDelete.py
+++ scripts/addons/Tila_Config/tila_OP_SmartDelete.py
@@ -14,36 +14,46 @@ class SmartDeleteOperator(bpy.types.Operator):
     bl_label = "TILA: Smart Delete"
     bl_options = {'REGISTER', 'UNDO'}
 
+    menu: bpy.props.BoolProperty(name='call_menu', default=False)
+
     def execute(self, context):
         if context.space_data.type == 'VIEW_3D':
-            if context.mode == 'EDIT_MESH':
-                current_mesh_mode = context.tool_settings.mesh_select_mode[:]
-                # if vertex mode on
-                if current_mesh_mode[0]:
-                    bpy.ops.mesh.dissolve_verts()
+            if self.menu:
+                if context.mode == 'EDIT_MESH':
+                    bpy.ops.wm.call_menu(name='VIEW3D_MT_edit_mesh_delete')
+                elif context.mode == 'EDIT_CURVE':
+                    bpy.ops.wm.call_menu(name='VIEW3D_MT_edit_curve_delete')
+            else:
+                if context.mode == 'EDIT_MESH':
+                    current_mesh_mode = context.tool_settings.mesh_select_mode[:]
+                    # if vertex mode on
+                    if current_mesh_mode[0]:
+                        bpy.ops.mesh.dissolve_verts()
 
-                # if vertex mode on
-                if current_mesh_mode[1]:
-                    bpy.ops.mesh.dissolve_edges(use_verts=True)
+                    # if vertex mode on
+                    if current_mesh_mode[1]:
+                        bpy.ops.mesh.dissolve_edges(use_verts=True)
 
-                # if vertex mode on
-                if current_mesh_mode[2]:
-                    bpy.ops.mesh.delete(type='FACE')
-            elif context.mode == 'EDIT_CURVE':
-                bpy.ops.curve.delete(type='VERT')
+                    # if vertex mode on
+                    if current_mesh_mode[2]:
+                        bpy.ops.mesh.delete(type='FACE')
+                elif context.mode == 'EDIT_CURVE':
+                    bpy.ops.curve.delete(type='VERT')
 
-            elif context.mode == 'EDIT_GPENCIL':
-                try:
-                    bpy.ops.gpencil.delete(type='POINTS')
-                except Exception as e:
-                    print("Warning: %r" % e)
-        
-            elif context.mode == 'OBJECT':
-                bpy.ops.object.delete(use_global=False, confirm=False)
+                elif context.mode == 'EDIT_GPENCIL':
+                    try:
+                        bpy.ops.gpencil.delete(type='POINTS')
+                    except Exception as e:
+                        print("Warning: %r" % e)
+            
+                elif context.mode == 'OBJECT':
+                    bpy.ops.object.delete(use_global=False, confirm=False)
 
         elif context.space_data.type == 'OUTLINER':
             bpy.ops.outliner.delete()
 
+        elif context.space_data.type == 'FILEBROWSER':
+            bpy.ops.file.delete()
         # elif context.space_data.type == 'IMAGE_EDITOR':
         #     layout.label("No Context! image editor")
         return {'FINISHED'}
