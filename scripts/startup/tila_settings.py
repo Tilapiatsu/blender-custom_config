f47529e8861476b66ed5c7e1b55bbfd744d581a9
Tilapiatsu <tilapiatsu@hotmail.fr>
2021-09-18 22:19:24 +0200

    * update default Tablet Drag Threshold

--- scripts/startup/tila_settings.py
+++ scripts/startup/tila_settings.py
@@ -75,6 +75,7 @@ bpy.context.preferences.inputs.view_zoom_axis = "HORIZONTAL"
 bpy.context.preferences.inputs.ndof_view_navigate_method = "FREE"
 bpy.context.preferences.inputs.view_rotate_method = "TRACKBALL"
 bpy.context.preferences.inputs.view_rotate_sensitivity_trackball = 2
+bpy.context.preferences.inputs.drag_threshold_tablet = 1
 bpy.context.preferences.inputs.ndof_view_rotate_method = "TRACKBALL"
 bpy.context.preferences.inputs.use_auto_perspective = True
 bpy.context.preferences.inputs.use_mouse_depth_navigate = True
