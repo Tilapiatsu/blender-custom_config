8e2020cd15ca3d7bcdf68d986ac40a0ddbc57c68
Tilapiatsu <tilapiatsu@hotmail.fr>
2022-01-26 11:58:28 +0100

    * reorder Pie Area

--- scripts/startup/tila_OP_pie_areas.py
+++ scripts/startup/tila_OP_pie_areas.py
@@ -21,36 +21,33 @@ class TILA_MT_pie_areas(Menu):
 
 	def draw(self, context):
 		layout = self.layout
-		pie = layout.menu_pie() 	
-		# left
-#   	 pie.operator("wm.preview_render", text="Preview Render")
+		pie = layout.menu_pie()
+
+		prop = pie.operator("wm.context_set_enum", text="Shader Editor")
+		prop.data_path = "area.type"
+		prop.value = 'NODE_EDITOR'  
+		
+		pie.operator("wm.3dview", text="3D View")
+
+		pie.operator("wm.uv_view", text="UV Editor")
+
+		pie.operator("screen.screen_full_area", text="Maximize")
+
+		prop = pie.operator("wm.context_set_enum", text="Python Console")
+		prop.data_path = "area.type"
+		prop.value = 'CONSOLE'
 
 		prop = pie.operator("wm.context_set_enum", text="Timeline")
 		prop.data_path = "area.type"
 		prop.value = 'DOPESHEET_EDITOR'
-		# right
-		pie.operator("wm.3dview", text="3D View")
-		# bottom
-		#block
-		prop = pie.operator("wm.context_set_enum", text="Python Console")
-		prop.data_path = "area.type"
-		prop.value = 'CONSOLE'  
-
-		# top
-		pie.operator("screen.screen_full_area", text="Maximize")
-
-		prop = pie.operator("wm.uv_view", text="UV Editor")
-
-		prop = pie.operator("wm.context_set_enum", text="Shader Editor")
-		prop.data_path = "area.type"
-		prop.value = 'NODE_EDITOR'  	  
+  
 		prop = pie.operator("wm.context_set_enum", text="Outliner")
 		prop.data_path = "area.type"
-		prop.value = 'OUTLINER'  	 
+		prop.value = 'OUTLINER'  	
+   
 		prop = pie.operator("wm.context_set_enum", text="Properties")
 		prop.data_path = "area.type"
-		prop.value = 'PROPERTIES'  	 
-#		pie.operator("wm.areas_popup", text="Outliner Popup").area = 'OUTLINER'
+		prop.value = 'PROPERTIES'
 
 class TILA_OT_areas_uv_view(bpy.types.Operator):
 	bl_idname = 'wm.uv_view'
