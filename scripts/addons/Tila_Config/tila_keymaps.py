37191619050b7003ef6466875854403d17cca34f
Tilapiatsu <tilapiatsu@hotmail.fr>
2022-05-30 14:10:00 +0200

    * Fix Boolean Keymap

--- scripts/addons/Tila_Config/tila_keymaps.py
+++ scripts/addons/Tila_Config/tila_keymaps.py
@@ -483,7 +483,7 @@ class TilaKeymaps(KeymapManager.KeymapManager):
 
 		self.tool_center(pivot='VIEW3D_PT_pivot_point', orientation='VIEW3D_PT_transform_orientations', action_center_context='VIEW3D')
 
-		self.kmi_set_replace('wm.call_menu_pie', 'Q', 'PRESS', ctrl=True, alt=True, shift=True, properties=[('name', 'HP_MT_boolean')])
+		self.kmi_set_replace('wm.call_menu_pie', 'Q', 'PRESS', ctrl=True, alt=True, shift=True, properties=[('name', 'HP_MT_pie_boolean')])
 		self.kmi_set_replace('view3d.toggle_wireframe', 'F5', 'PRESS', properties=[('mode', 'OVERLAY'), ('selected', False)], disable_double=True)
 		self.kmi_set_replace('view3d.toggle_wireframe', 'F5', 'PRESS', shift=True, properties=[('mode', 'OVERLAY'), ('selected', True)], disable_double=True)
 		self.kmi_set_replace('view3d.toggle_wireframe', 'F5', 'PRESS', ctrl=True, properties=[('mode', 'SET'), ('selected', False)], disable_double=True)
