5a22c88e327d7c0dcb22fd473814016429656594
Tilapiatsu <tilapiatsu@hotmail.fr>
2022-11-23 12:39:30 +0100

    * resize only if filename contains a string

--- scripts/helpers/image_resize.py
+++ scripts/helpers/image_resize.py
@@ -3,14 +3,22 @@ import os
 # Import Image
 
 # source_folder = r'E:\Projects\RND\TextureConversion'
-source_folder = r'D:\Folder'
+source_folder = r'C:\Folder'
 source_image_format = '.png'
-destination_resolution = (4096, 2048)
+destination_resolution = (2048, 4096)
+use_contains = True
+contains = 'SubString'
 
 for i in os.listdir(source_folder):
     filename, extension = os.path.splitext(i)
     if extension != source_image_format:
+        print(f'Skipping {i}')
         continue
+    if use_contains:
+        if contains not in filename:
+            print(filename)
+            print(f'Skipping {i}')
+            continue
 
     print(f'Resizing file {i} to {destination_resolution}')
     image = Image.open(os.path.join(source_folder, i))
