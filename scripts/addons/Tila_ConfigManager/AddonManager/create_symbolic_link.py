d585f176801e00c91736d3e4fb4c546d3988135d
Tilapiatsu <tilapiatsu@hotmail.fr>
2023-03-27 22:17:07 +0200

    - remove dependency of BPY

--- scripts/addons/Tila_ConfigManager/AddonManager/create_symbolic_link.py
+++ scripts/addons/Tila_ConfigManager/AddonManager/create_symbolic_link.py
@@ -1,16 +1,13 @@
 import sys
 import argparse
 import os
-from Logger import Logger
 
 
 class CreateSymbolicLink():
 	def __init__(self, argv=''):
-		self.log = Logger('CreateSymbolicLink')
 		self.parse_argsv(argv[argv.index("--") + 1:])
 	
 	def parse_argsv(self, argv):
-		self.log.info(f'Parsing Args')
 		parser = argparse.ArgumentParser(
 			description='This command allow you to create symbolic link with admin rights')
 		
@@ -28,7 +25,6 @@ class CreateSymbolicLink():
 
 	def create_symbolic_link(self):
 		for f in self.file_to_link:
-			self.log.info(f'Linking {f[0]} to {f[1]} {f[2]}')
 			os.symlink(f[0], f[1], target_is_directory=f[2])
 		
 if __name__ == '__main__':
