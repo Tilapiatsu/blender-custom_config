7fc4e01502abc0c52874948d336b328a8ea30279
Tilapiatsu <tilapiatsu@hotmail.fr>
2023-04-09 14:50:04 +0200

    * update Keymaps

--- scripts/addons/Tila_ConfigManager/AddonManager/keymaps.py
+++ scripts/addons/Tila_ConfigManager/AddonManager/keymaps.py
@@ -1473,7 +1473,7 @@ class TILA_Config_Keymaps_rotate_an_hdri(TILA_Config_Keymaps):
 		if self.kmi_init(name='3D View', space_type='VIEW_3D', region_type='WINDOW', addon=True, restore_to_default=False):
 			self.kmi_set_replace('rotate.hdri', self.k_context, 'CLICK_DRAG',
 			                     ctrl=True, alt=True, shift=False,  disable_double=True)
-			self.kmi_set_active(enable=True, idname='rotate.hdri')
+			# self.kmi_set_active(enable=True, idname='rotate.hdri')
 
 		self.print_status(f"Assignment of {self.addon_name} complete", start=False)
 		
