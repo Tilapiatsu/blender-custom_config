f9b88050d38621f26770dd26a6dd66ef835efe17
Tilapiatsu <tilapiatsu@hotmail.fr>
2023-04-15 11:25:48 +0200

    * Optimize

--- scripts/startup/tila_OP_brush_select_and_paint.py
+++ scripts/startup/tila_OP_brush_select_and_paint.py
@@ -22,7 +22,7 @@ class TILA_brush_select_and_paint(bpy.types.Operator):
     compatible_tools = ['SCULPT', 'VERTEX', 'WEIGHT', 'IMAGE', 'GPENCIL']
 
     initial_brush = None
-
+    brush_is_set = False
     press = False
 
     @property
@@ -44,50 +44,73 @@ class TILA_brush_select_and_paint(bpy.types.Operator):
     def set_initial_brush(self):
         if self.tool == 'SCULPT':
             brush = bpy.context.tool_settings.sculpt.brush
-            self.initial_brush = {'name':brush.name, 'weight':brush.weight}
+            self.initial_brush = {'name':brush.name, 'weight':brush.weight, 'strength':brush.strength}
         if self.tool == 'VERTEX':
             brush = bpy.context.tool_settings.vertex_paint.brush
-            self.initial_brush = {'name':brush.name, 'weight':brush.weight}
+            self.initial_brush = {'name':brush.name, 'weight':brush.weight, 'strength':brush.strength}
         if self.tool == 'WEIGHT':
             brush = bpy.context.tool_settings.weight_paint.brush
-            self.initial_brush = {'name':brush.name, 'weight':brush.weight}
+            self.initial_brush = {'name':brush.name, 'weight':brush.weight, 'strength':brush.strength}
         if self.tool == 'IMAGE':
             brush = bpy.context.tool_settings.image_paint.brush
-            self.initial_brush = {'name':brush.name, 'weight':brush.weight}
+            self.initial_brush = {'name':brush.name, 'weight':brush.weight, 'strength':brush.strength}
         if self.tool == 'GPENCIL':
             pass
+
+    def set_brush_settings(self, mode, brush):
+        if self.tool == 'SCULPT':
+            bpy.ops.paint.brush_select('INVOKE_DEFAULT', sculpt_tool=mode, toggle=False)
+            bpy.context.tool_settings.sculpt.brush = bpy.data.brushes[brush]
+            bpy.context.tool_settings.sculpt.brush.weight = self.initial_brush['weight']
+            bpy.context.tool_settings.sculpt.brush.strength = self.initial_brush['strength']
+            self.brush_is_set = True
+            
+        elif self.tool == 'VERTEX':
+            bpy.ops.paint.brush_select('INVOKE_DEFAULT', vertex_tool=mode, toggle=False)
+            bpy.context.tool_settings.vertex_paint.brush = bpy.data.brushes[brush]
+            bpy.context.tool_settings.vertex_paint.brush.weight = self.initial_brush['weight']
+            bpy.context.tool_settings.vertex_paint.brush.strength = self.initial_brush['strength']
+            self.brush_is_set = True
+
+        elif self.tool == 'WEIGHT':
+            bpy.ops.paint.brush_select('INVOKE_DEFAULT', weight_tool=mode, toggle=False)
+            bpy.context.tool_settings.weight_paint.brush = bpy.data.brushes[brush]
+            bpy.context.tool_settings.weight_paint.brush.weight = self.initial_brush['weight']
+            bpy.context.tool_settings.weight_paint.brush.strength = self.initial_brush['strength']
+            self.brush_is_set = True
+
+        elif self.tool == 'IMAGE':
+            bpy.ops.paint.brush_select('INVOKE_DEFAULT', image_tool=mode, toggle=False)
+            bpy.context.tool_settings.image_paint.brush = bpy.data.brushes[brush]
+            bpy.context.tool_settings.image_paint.brush.weight = self.initial_brush['weight']
+            bpy.context.tool_settings.image_paint.brush.strength = self.initial_brush['strength']
+            self.brush_is_set = True
+
+        elif self.tool == 'GPENCIL':
+            bpy.ops.paint.brush_select('INVOKE_DEFAULT', gpencil_tool=mode, toggle=False)
+            self.brush_is_set = True
 
     def run_tool(self, mode, brush):
         try:
             if self.tool not in self.compatible_tools:
                 return {'CANCELLED'}
             
+            if not self.brush_is_set:
+                self.set_brush_settings(mode, brush)
+            
             if self.tool == 'SCULPT':
-                bpy.ops.paint.brush_select('INVOKE_DEFAULT', sculpt_tool=mode, toggle=False)
-                bpy.context.tool_settings.sculpt.brush = bpy.data.brushes[brush]
-                bpy.context.tool_settings.sculpt.brush.weight = self.initial_brush['weight']
                 if mode != self.default_mode or brush != self.initial_brush['name']:
                     bpy.ops.sculpt.brush_stroke('INVOKE_DEFAULT')
             if self.tool == 'VERTEX':
-                bpy.ops.paint.brush_select('INVOKE_DEFAULT', vertex_tool=mode, toggle=False)
-                bpy.context.tool_settings.vertex_paint.brush = bpy.data.brushes[brush]
-                bpy.context.tool_settings.vertex_paint.brush.weight = self.initial_brush['weight']
                 if mode != self.default_mode or brush != self.initial_brush['name']:
                     bpy.ops.paint.vertex_paint('INVOKE_DEFAULT')
             if self.tool == 'WEIGHT':
-                bpy.ops.paint.brush_select('INVOKE_DEFAULT', weight_tool=mode, toggle=False)
-                bpy.context.tool_settings.weight_paint.brush = bpy.data.brushes[brush]
-                bpy.context.tool_settings.weight_paint.brush.weight = self.initial_brush['weight']
                 if mode != self.default_mode or brush != self.initial_brush['name']:
                     bpy.ops.paint.weight_paint('INVOKE_DEFAULT')
             if self.tool == 'IMAGE':
-                bpy.ops.paint.brush_select('INVOKE_DEFAULT', image_tool=mode, toggle=False)
-                bpy.context.tool_settings.image_paint.brush = bpy.data.brushes[brush]
-                bpy.context.tool_settings.image_paint.brush.weight = self.initial_brush['weight']
                 if mode != self.default_mode or brush != self.initial_brush['name']:
                     bpy.ops.paint.image_paint('INVOKE_DEFAULT')
             if self.tool == 'GPENCIL':
-                bpy.ops.paint.brush_select('INVOKE_DEFAULT', gpencil_tool=mode, toggle=False)
                 if mode != self.default_mode or brush != self.initial_brush['name']:
                     bpy.ops.gpencil.draw('INVOKE_DEFAULT')
 
@@ -96,15 +119,17 @@ class TILA_brush_select_and_paint(bpy.types.Operator):
 
     def modal(self, context, event):
         if event.type in {'ESC'}:  # Cancel
+            self.brush_is_set = False
             self.run_tool(self.default_mode, self.initial_brush['name'])
+            self.brush_is_set = False
             return {'CANCELLED'}
         elif self.get_release_condition(event=event):
-            print('release')
             self.press = False
+            self.brush_is_set = False
             self.run_tool(self.default_mode, self.initial_brush['name'])
+            self.brush_is_set = False
             return{'FINISHED'}
         elif self.get_run_condition(event=event):
-            print('press')
             self.press = True
             self.run_tool(self.mode, self.brush)
         return {'RUNNING_MODAL'}
