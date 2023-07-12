adccfc045c6fe401c45a49d0393fdb1aaf63aad6
Tilapiatsu <tilapiatsu@hotmail.fr>
2023-07-13 00:56:06 +0200

    * try to get Max row height

--- scripts/helpers/compose_grid_image.py
+++ scripts/helpers/compose_grid_image.py
@@ -2,12 +2,24 @@ from PIL import Image
 import os
 # Import Image
 
-source_folder = r'D:\path'
+source_folder = r'C:\Users\lhlau\Documents\Tilapiatsu\Projects\RND\GridImage'
 source_image_format = '.png'
 destination_resolution = (2048, 4096)
-use_contains = True
+use_contains = False
 contains = 'filename'
 max_row_size = 4
+crop_overscan = 0.1
+
+def get_max_row_height(image_to_process, max_row_size):
+    original_crops =  image_to_process.values()
+    max_crops = []
+    for i in range(len(original_crops)):
+        if i % max_row_size == 0:
+            max_crops.append(min(original_crops[i]))
+
+
+def get_max_collumn_height(image_to_process, max_row_size):
+    pass
 
 images_to_process = []
 for i in os.listdir(source_folder):
@@ -20,14 +32,23 @@ for i in os.listdir(source_folder):
             print(filename)
             print(f'Skipping {i}')
             continue
+
     image_path = os.path.join(source_folder, i)
     with Image.open(image_path) as image:
         bbox = image.getbbox()
         # Store Image Path and BBox to crop later
-        images_to_process.append({image_path:bbox + bbox * 0.1})
+        images_to_process.append({image_path : bbox})
 
-for image_path in images_to_process:
+row_number = 0
+image_number = 0
 
+for image_path, image_bbox in images_to_process.items():
+    image_number += 1
+
+    if image_number % max_row_size == 0:
+        row_number += 1
+    
+    
     print(f'Resizing file {i} to {destination_resolution}')
     resized_image = image.resize(destination_resolution, resample = Image.Resampling.LANCZOS)
 
