16c2163224321037d4724d578637c9e946388a05
Tilapiatsu <tilapiatsu@hotmail.fr>
2023-07-13 10:07:44 +0200

    * getting max crop values working

--- scripts/helpers/compose_grid_image.py
+++ scripts/helpers/compose_grid_image.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os
 # Import Image
 
-source_folder = r'C:\Users\lhlau\Documents\Tilapiatsu\Projects\RND\GridImage'
+source_folder = r'D:\path'
 source_image_format = '.png'
 destination_resolution = (2048, 4096)
 use_contains = False
@@ -10,12 +10,23 @@ contains = 'filename'
 max_row_size = 4
 crop_overscan = 0.1
 
-def get_max_row_height(image_to_process, max_row_size):
-    original_crops =  image_to_process.values()
+def get_max_row_height(images_to_process, max_row_size):
+    original_crops =  [i[1] for i in images_to_process]
+    print('images_to_process', images_to_process)
+    print('original_crops', original_crops)
     max_crops = []
     for i in range(len(original_crops)):
         if i % max_row_size == 0:
-            max_crops.append(min(original_crops[i]))
+            command = 'max_crops.append(min('
+            for j in range(0,max_row_size):
+                command += f'original_crops[{i+j}]'
+                if j < max_row_size-1:
+                    command += ', '
+                else:
+                    command += '))'
+            print(command)
+            exec(command)
+    return max_crops
 
 
 def get_max_collumn_height(image_to_process, max_row_size):
@@ -37,20 +48,21 @@ for i in os.listdir(source_folder):
     with Image.open(image_path) as image:
         bbox = image.getbbox()
         # Store Image Path and BBox to crop later
-        images_to_process.append({image_path : bbox})
+        images_to_process.append((image_path, bbox))
 
+print(get_max_row_height(images_to_process, max_row_size))
 row_number = 0
 image_number = 0
 
-for image_path, image_bbox in images_to_process.items():
+for image in images_to_process:
     image_number += 1
 
     if image_number % max_row_size == 0:
         row_number += 1
     
     
-    print(f'Resizing file {i} to {destination_resolution}')
-    resized_image = image.resize(destination_resolution, resample = Image.Resampling.LANCZOS)
+    # print(f'Resizing file {i} to {destination_resolution}')
+    # resized_image = image.resize(destination_resolution, resample = Image.Resampling.LANCZOS)
 
-    resized_image.save(os.path.join(source_folder, i))
+    # resized_image.save(os.path.join(source_folder, i))
 
