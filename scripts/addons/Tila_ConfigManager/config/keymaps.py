92deaf8a8cc92f0dc5429a5082c3249d45e799a7
Tilapiatsu <tilapiatsu@hotmail.fr>
2024-10-30 09:48:14 +0100

    * use topology distance for Pick Shortest Path

--- scripts/addons/Tila_ConfigManager/config/keymaps.py
+++ scripts/addons/Tila_ConfigManager/config/keymaps.py
@@ -231,14 +231,14 @@ class TILA_Config_Keymaps_Global(TILA_Config_Keymaps):
         #  shortest Path Select / Deselect / Add
         if shortestpath_tool:
             self.kmi_remove(idname=shortestpath_tool)
-            use_topology_distance = False
+            use_topology_distance = True
                 
             self.kmi_set_replace(shortestpath_tool, self.k_context, 'CLICK', shift=True, disable_double=True, properties={'use_fill': False, 'use_face_step': False, 'use_topology_distance': use_topology_distance})
             self.kmi_set_replace(shortestpath_tool, self.k_context, 'CLICK', ctrl=True, shift=True, disable_double=True, properties={'use_fill': True, 'use_face_step': False, 'use_topology_distance': use_topology_distance})
 
         #  shortest ring
         if shortestring_tool:
-            self.kmi_set_replace(shortestring_tool, self.k_cursor, 'CLICK', shift=True, disable_double=True, properties={'use_fill': False, 'use_face_step': True, 'use_topology_distance': False})
+            self.kmi_set_replace(shortestring_tool, self.k_cursor, 'CLICK', shift=True, disable_double=True, properties={'use_fill': False, 'use_face_step': True, 'use_topology_distance': True})
 
         # Loop Select / Deselect / Add
         if loop_tool:
