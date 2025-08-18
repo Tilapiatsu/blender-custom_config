854963b27732d8173bf36376f2d0eef5b1cc3975
Tilapiatsu <tilapiatsu@hotmail.fr>
2025-08-18 21:58:40 +0200

    * Simplify empty mesh script

--- scripts/startup/tila_OP_EmptyMesh.py
+++ scripts/startup/tila_OP_EmptyMesh.py
@@ -18,29 +18,9 @@ class TILA_EmptyMeshOperator(bpy.types.Operator):
     emptymesh_name = 'tila_emptymesh'
 
     def execute(self, context):
-        currentActiveObject = bpy.context.active_object
-        if currentActiveObject:
-            currentMode = currentActiveObject.mode
-        else:
-            currentMode = "OBJECT"
-
-        currentSelection = bpy.context.selected_objects
-
-        if currentMode == "EDIT":
-            bpy.ops.object.mode_set(mode='OBJECT')
-
-        bpy.ops.mesh.primitive_plane_add(align='WORLD', enter_editmode=False, location=(0.0, 0.0, 0.0))
-        bpy.context.object.data.name = self.emptymesh_name
-        bpy.ops.object.mode_set(mode='EDIT')
-        bpy.ops.mesh.select_all(action='SELECT')
-        bpy.ops.mesh.delete(type='FACE')
-        bpy.ops.object.mode_set(mode='OBJECT')
-
-        bpy.context.selected_objects[0].name = "EmptyMesh"
-
-        if currentMode == "EDIT":
-            bpy.ops.object.mode_set(mode='EDIT')
-
+        mesh = bpy.data.meshes.new(name=self.emptymesh_name)
+        obj = bpy.data.objects.new(name=self.emptymesh_name, object_data=mesh)
+        bpy.context.collection.objects.link(obj)
         return {'FINISHED'}
 
 
