f245221a1caa71de1d92a6b938265244b8aeab5c
Tilapiatsu <tilapiatsu@hotmail.fr>
2025-08-19 11:18:25 +0200

    * switch empty mesh in edit mode after creation if it was in edit mode before running the command

--- scripts/startup/tila_OP_EmptyMesh.py
+++ scripts/startup/tila_OP_EmptyMesh.py
@@ -18,13 +18,27 @@ class TILA_EmptyMeshOperator(bpy.types.Operator):
     emptymesh_name = 'tila_emptymesh'
 
     def execute(self, context):
+        current_active_object = bpy.context.active_object
+        if current_active_object:
+            currentMode = current_active_object.mode
+        else:
+            currentMode = "OBJECT"
+
+        if currentMode == "EDIT":
+            bpy.ops.object.mode_set(mode='OBJECT')
+
         mesh = bpy.data.meshes.new(name=self.emptymesh_name)
         obj = bpy.data.objects.new(name=self.emptymesh_name, object_data=mesh)
         bpy.context.collection.objects.link(obj)
+
         if context.mode == 'OBJECT':
             bpy.ops.object.select_all(action='DESELECT')
             bpy.context.view_layer.objects.active = obj
             bpy.data.objects[obj.name].select_set(True)
+
+        if currentMode == "EDIT":
+            bpy.ops.object.mode_set(mode='EDIT')
+
         return {'FINISHED'}
 
 
