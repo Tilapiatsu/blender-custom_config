3a8145eff16717f7332b11fd13e981568c83341d
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-01-31 23:21:01 +0100

    * update uv loop select

--- scripts/startup/tila_OP_uv_select_loop.py
+++ scripts/startup/tila_OP_uv_select_loop.py
@@ -9,7 +9,7 @@ bl_info = {
     "location": "",
     "warning": "",
     "doc_url": "",
-    "category": "3D View"
+    "category": "3D View",
 }
 
 
@@ -17,27 +17,36 @@ class TILA_uv_select(bpy.types.Operator):
     bl_idname = "uv.tila_uv_select"
     bl_label = "TILA: UV select"
 
-    extend : bpy.props.BoolProperty(name="Extend", default=False)
-    loop : bpy.props.BoolProperty(name="Loop", default=False)
+    extend: bpy.props.BoolProperty(name="Extend", default=False)
+    loop: bpy.props.BoolProperty(name="Loop", default=False)
+    deselect: bpy.props.BoolProperty(name="Deselect", defaul=False)
+    deselect_all: bpy.props.BoolProperty(name="Deselect All", default=False)
+    toggle: bpy.props.BoolProperty(name="Toggle", defaul=False)
 
     @classmethod
     def poll(cls, context):
-        return context.mode == 'EDIT_MESH'
+        return context.mode == "EDIT_MESH"
 
     def execute(self, context):
         command = bpy.ops.uv.select
         if self.loop:
             command = bpy.ops.uv.select_loop
+            command("INVOKE_DEFAULT", extend=self.extend)
+        else:
+            command(
+                "INVOKE_DEFAULT",
+                extend=self.extend,
+                deselect=self.deselect,
+                deselect_all=self.deselect_all,
+                toggle=self.toggle,
+            )
 
-        command('INVOKE_DEFAULT', extend=self.extend)
-        if self.loop and bpy.context.scene.tool_settings.uv_select_mode != 'VERTEX':
-            command('INVOKE_DEFAULT', extend=self.extend)
-        return {'FINISHED'}
+        if self.loop and bpy.context.scene.tool_settings.uv_select_mode != "VERTEX":
+            command("INVOKE_DEFAULT", extend=self.extend)
+        return {"FINISHED"}
 
 
-classes = (
-    TILA_uv_select,
-)
+classes = (TILA_uv_select,)
 
 
 register, unregister = bpy.utils.register_classes_factory(classes)
