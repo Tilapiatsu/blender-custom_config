d3b208c0e1019e52a5ad8d88bb405e721ee1e51c
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-03-27 10:24:34 +0100

    * Fix mask_faceset

--- scripts/startup/tila_OP_sculpt_mask_faceset.py
+++ scripts/startup/tila_OP_sculpt_mask_faceset.py
@@ -1,43 +1,44 @@
 import bpy
 
 bl_info = {
-	"name": "Tila : Sculpt Mask FaceSet",
-	"author": "Tilapiatsu",
-	"version": (1, 0, 0, 0),
-	"blender": (2, 80, 0),
-	"location": "View3D",
-	"category": "Sculpt",
+    "name": "Tila : Sculpt Mask FaceSet",
+    "author": "Tilapiatsu",
+    "version": (1, 0, 0, 0),
+    "blender": (2, 80, 0),
+    "location": "View3D",
+    "category": "Sculpt",
 }
+
 
 class TILA_SculptMaskFaceSet(bpy.types.Operator):
     bl_idname = "sculpt.tila_mask_faceset"
     bl_label = "Mask/Unmask Faceset under the cursor"
 
-    mode : bpy.props.EnumProperty(name="mode", items=[("MASK", "Mask", ""), ("TOGGLE", "Toggle", "")])
+    mode: bpy.props.EnumProperty(name="mode", items=[("MASK", "Mask", ""), ("TOGGLE", "Toggle", "")])
 
     def invoke(self, context, event):
-        if self.mode == 'MASK':
-            bpy.ops.paint.hide_show_all(action='SHOW')
-            bpy.ops.paint.mask_flood_fill(mode='VALUE', value=0.0)
-            bpy.ops.sculpt.face_set_change_visibility(mode='TOGGLE')
-            bpy.ops.paint.mask_flood_fill(mode='INVERT')
-            bpy.ops.paint.hide_show_all(action='SHOW')
-            bpy.ops.paint.mask_flood_fill(mode='INVERT')
-        elif self.mode == 'TOGGLE':
-            bpy.ops.paint.hide_show_all(action='SHOW')
-            bpy.ops.paint.mask_flood_fill(mode='INVERT')
-            bpy.ops.sculpt.face_set_change_visibility(mode='TOGGLE')
-            bpy.ops.paint.mask_flood_fill(mode='INVERT')
-            bpy.ops.paint.hide_show_all(action='SHOW')
-            bpy.ops.paint.mask_flood_fill(mode='INVERT')
+        if self.mode == "MASK":
+            # print("Masking Faceset")
+            bpy.ops.paint.hide_show_all(action="SHOW")
+            bpy.ops.paint.mask_flood_fill(mode="VALUE", value=0.0)
+            bpy.ops.sculpt.face_set_change_visibility("INVOKE_DEFAULT", mode="TOGGLE", active_face_set=0)
+            bpy.ops.paint.mask_flood_fill(mode="INVERT")
+            bpy.ops.paint.hide_show_all(action="SHOW")
+            bpy.ops.paint.mask_flood_fill(mode="INVERT")
+        elif self.mode == "TOGGLE":
+            # print("Toggle Faceset")
+            bpy.ops.paint.hide_show_all(action="SHOW")
+            bpy.ops.paint.mask_flood_fill(mode="INVERT")
+            bpy.ops.sculpt.face_set_change_visibility(mode="TOGGLE")
+            bpy.ops.paint.mask_flood_fill(mode="INVERT")
+            bpy.ops.paint.hide_show_all(action="SHOW")
+            bpy.ops.paint.mask_flood_fill(mode="INVERT")
             # bpy.ops.sculpt.mask_filter(filter_type='GROW', auto_iteration_count=True)
             # bpy.ops.sculpt.mask_filter(filter_type='SHRINK', auto_iteration_count=True)
-        return{'FINISHED'}
+        return {"FINISHED"}
 
 
-classes = (
-    TILA_SculptMaskFaceSet,
-)
+classes = (TILA_SculptMaskFaceSet,)
 
 
 register, unregister = bpy.utils.register_classes_factory(classes)
@@ -45,3 +46,4 @@ register, unregister = bpy.utils.register_classes_factory(classes)
 
 if __name__ == "__main__":
     register()
+
