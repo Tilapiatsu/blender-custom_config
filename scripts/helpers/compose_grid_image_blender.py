e0b94d8183fa6a68383d7e0eec4e26a6c945152f
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-04-22 23:24:21 +0200

    * fix column calculation when max_row_size is exceeded

--- scripts/helpers/compose_grid_image_blender.py
+++ scripts/helpers/compose_grid_image_blender.py
@@ -1,11 +1,12 @@
 try:
     from PIL import Image
-    
+
 except ModuleNotFoundError as e:
     print(e)
     import subprocess
     import sys
-    print(subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pillow']))
+
+    print(subprocess.check_call([sys.executable, "-m", "pip", "install", "pillow"]))
     from PIL import Image
 
 import os
@@ -14,14 +15,14 @@ import bpy
 
 
 ### Parameters
-source_folder = bpy.path.abspath('c:\\path')
-source_image_format = '.png'
+source_folder = bpy.path.abspath("c:\\path")
+source_image_format = ".png"
 max_destination_resolution = (6000, 6000)
 background_color = (0, 0, 0, 1)
 use_contains = True
 save_jpeg = True
 save_png = True
-contains = 'contains'
+contains = "contains"
 output_filename = contains
 max_row_size = 4
 crop_overscan = 0.02
@@ -31,25 +32,28 @@ normalize_size = (True, False)
 def get_max_row_height(images_to_process, max_row_size):
     original_crops = [i[1] for i in images_to_process]
     max_crops = []
+    nb_images = len(images_to_process)
     for i in range(len(original_crops)):
         if i % max_row_size == 0:
             top = 0
             bottom = 0
-            top_command = r'min('
-            bottom_command = r'max('
+            top_command = r"min("
+            bottom_command = r"max("
             for j in range(max_row_size):
-                index = min(i+j, len(original_crops)-1)
-                top_command += f'original_crops[{index}][1]'
-                bottom_command += f'original_crops[{index}][3]'
-                if index < i+j:
-                    top_command += ')'
-                    bottom_command += ')'
-                elif j < max_row_size-1:
-                    top_command += ', '
-                    bottom_command += ', '
+                if j > nb_images:
+                    continue
+                index = min(i + j, len(original_crops) - 1)
+                top_command += f"original_crops[{index}][1]"
+                bottom_command += f"original_crops[{index}][3]"
+                if index < i + j:
+                    top_command += ")"
+                    bottom_command += ")"
+                elif j < max_row_size - 1:
+                    top_command += ", "
+                    bottom_command += ", "
                 else:
-                    top_command += ')'
-                    bottom_command += ')'
+                    top_command += ")"
+                    bottom_command += ")"
             top = eval(top_command)
             bottom = eval(bottom_command)
             max_crops.append((top, bottom))
@@ -63,26 +67,27 @@ def get_max_collumn_width(images_to_process, max_row_size):
     row_count = math.ceil(len(original_crops) / max_row_size)
     for i in range(max_row_size):
         if row_count > 1:
-            left_command = r'min('
-            right_command = r'max('
+            left_command = r"min("
+            right_command = r"max("
         else:
-            left_command = r'('
-            right_command = r'('
+            left_command = r"("
+            right_command = r"("
         left = 0
         right = 0
         for j in range(row_count):
-            index = min(i + max_row_size * j, len(original_crops)-1)
-            left_command += f'original_crops[{index}][0]'
-            right_command += f'original_crops[{index}][2]'
-            if index < i + max_row_size * j -1:
-                left_command += ')'
-                right_command += ')'
-            elif j < row_count-1:
-                left_command += ', '
-                right_command += ', '
+            index = min(i + max_row_size * j, len(original_crops) - 1)
+            left_command += f"original_crops[{index}][0]"
+            right_command += f"original_crops[{index}][2]"
+            if index < i + max_row_size * j - 1:
+                left_command += ")"
+                right_command += ")"
+            elif j < row_count - 1:
+                left_command += ", "
+                right_command += ", "
             else:
-                left_command += ')'
-                right_command += ')'
+                left_command += ")"
+                right_command += ")"
+
         left = eval(left_command)
         right = eval(right_command)
 
@@ -97,15 +102,15 @@ def get_images_to_process(source_folder):
     for i in os.listdir(source_folder):
         filename, extension = os.path.splitext(i)
         if extension != source_image_format:
-            print(f'Skipping {i}')
+            print(f"Skipping {i}")
             continue
         if use_contains:
             if contains not in filename:
                 print(filename)
-                print(f'Skipping {i}')
+                print(f"Skipping {i}")
                 continue
-        if filename == output_filename + '_composite':
-            print(f'Skipping {i}')
+        if filename == output_filename + "_composite":
+            print(f"Skipping {i}")
             continue
 
         image_path = os.path.join(source_folder, i)
@@ -113,19 +118,23 @@ def get_images_to_process(source_folder):
             bbox = image.getbbox()
             # Store Image Path and BBox to crop later
             images_to_process.append((image_path, bbox))
-    
+
     _, bboxes = zip(*images_to_process)
     _, _, width, height = zip(*bboxes)
     max_width = max(*width)
     max_height = max(*height)
-    
+
     normalized_images_to_process = []
-    
-    for i, e in enumerate(images_to_process):        
+
+    for i, e in enumerate(images_to_process):
         if normalize_size[0]:
-            normalized_images_to_process.append( (e[0], (e[1][0], e[1][1], max_width, math.ceil(e[1][3] * max_width / e[1][2]))))
+            normalized_images_to_process.append(
+                (e[0], (e[1][0], e[1][1], max_width, math.ceil(e[1][3] * max_width / e[1][2])))
+            )
         elif normalize_size[1]:
-            normalized_images_to_process.append( (e[0], (e[1][0], e[1][1], math.ceil(e[1][2] * max_height / e[1][3]), max_height)))
+            normalized_images_to_process.append(
+                (e[0], (e[1][0], e[1][1], math.ceil(e[1][2] * max_height / e[1][3]), max_height))
+            )
         else:
             normalized_images_to_process.append(e)
 
@@ -140,24 +149,28 @@ def get_image_size(images_to_process, max_row_size, max_row_height, max_collumn_
     with Image.open(image_path) as image:
         uncroped_image_size = (image.size[0], image.size[1])
 
-    print('uncroped', uncroped_image_size)
+    print("uncroped", uncroped_image_size)
 
     for i in range(len(images_to_process)):
         height_crop = (0, 0)
         if i < max_row_size:
-            width_crop =  max_collumn_width[i % max_row_size]
+            width_crop = max_collumn_width[i % max_row_size]
 
-        if i % max_row_size == 0: # if new row
+        if i % max_row_size == 0:  # if new row
             height_crop = max_row_height[math.floor(i / max_row_size)]
 
-        if i < max_row_size :
-            image_size = (	math.floor((image_size[0] - width_crop[0] + width_crop[1]) * (1 + crop_overscan)),
-                            image_size[1])
-            
-        image_size = (	image_size[0],
-                        math.floor((image_size[1] - height_crop[0] + height_crop[1]) * (1 + crop_overscan))  )
-        
-        print('image_size_iter =', image_size)
+        if i < max_row_size:
+            image_size = (
+                math.floor((image_size[0] - width_crop[0] + width_crop[1]) * (1 + crop_overscan)),
+                image_size[1],
+            )
+
+        image_size = (
+            image_size[0],
+            math.floor((image_size[1] - height_crop[0] + height_crop[1]) * (1 + crop_overscan)),
+        )
+
+        print("image_size_iter =", image_size)
 
     return image_size
 
@@ -166,38 +179,39 @@ def get_resized_dimensions(image_size, max_size):
     new_width = image_size[0]
     new_height = image_size[1]
     if new_width > max_size[0]:
-        print(f'width_too_big = {image_size[0]} / {max_size[0]}')
+        print(f"width_too_big = {image_size[0]} / {max_size[0]}")
         new_width = max_size[0]
         new_height = math.floor(image_size[1] * max_size[0] / image_size[0])
 
     if new_height > max_size[1]:
-        print(f'height_too_big = {image_size[1]} / {max_size[1]}')
+        print(f"height_too_big = {image_size[1]} / {max_size[1]}")
         new_width = math.floor(image_size[0] * max_size[1] / image_size[1])
         new_height = max_size[1]
 
-    print(f'resized_dimensions = ({new_width}, {new_height})')
+    print(f"resized_dimensions = ({new_width}, {new_height})")
     return (new_width, new_height)
-    
+
 
 images_to_process = get_images_to_process(source_folder)
 
 for i in range(len(images_to_process)):
-    print(f'images_to_process_{i} =', images_to_process[i][0])
-    print(f'bbox_{i} = ', images_to_process[i][1])
+    print(f"images_to_process_{i} =", images_to_process[i][0])
+    print(f"bbox_{i} = ", images_to_process[i][1])
+
 
 max_row_height = get_max_row_height(images_to_process, max_row_size)
 max_collumn_width = get_max_collumn_width(images_to_process, max_row_size)
 
-print('max_row_height =', max_row_height)
-print('max_collumn_width =', max_collumn_width)
+print("max_row_height =", max_row_height)
+print("max_collumn_width =", max_collumn_width)
 
 image_size = get_image_size(images_to_process, max_row_size, max_row_height, max_collumn_width)
-print('image_size =', image_size)
+print("image_size =", image_size)
 
 row_number = 0
 image_number = 0
 image_pathes = [i[0] for i in images_to_process]
-final_image = Image.new('RGBA', image_size, background_color)
+final_image = Image.new("RGBA", image_size, background_color)
 paste_position = (0, 0, 0, 0)
 new_row = 0
 
@@ -208,33 +222,36 @@ for i, current_image_to_process in enumerate(images_to_process):
     image_path, size = current_image_to_process
     if image_number != 0:
         if image_number % max_row_size == 0:
-            row_number +=1
+            row_number += 1
             paste_position = (0, paste_position[3], 0, 0)
 
     with Image.open(image_path) as image:
         if normalize_size[0] or normalize_size[1]:
             image = image.resize((size[2], size[3]))
         height_crop = max_row_height[math.floor(image_number / max_row_size)]
-        width_crop =  max_collumn_width[image_number % max_row_size]
-        
-        cropped = image.crop((	width_crop[0], 
-                                height_crop[0],
-                                width_crop[1],
-                                height_crop[1]))
+        width_crop = max_collumn_width[image_number % max_row_size]
 
-        print(f'crop_size_{image_number} =', cropped.size)
+        cropped = image.crop((width_crop[0], height_crop[0], width_crop[1], height_crop[1]))
+
+        print(f"crop_size_{image_number} =", cropped.size)
         if image_number % max_row_size == 0:
-            paste_position = (	paste_position[0], # Left 0
-                                paste_position[1] + math.floor(image_size[1] / max_row_size * crop_overscan * 8), # Top 1
-                                paste_position[2], # Right 2
-                                paste_position[1] + cropped.size[1] + math.floor(image_size[1] / max_row_size * crop_overscan * 8)) # Bottom 3
+            paste_position = (
+                paste_position[0],  # Left 0
+                paste_position[1] + math.floor(image_size[1] / max_row_size * crop_overscan * 8),  # Top 1
+                paste_position[2],  # Right 2
+                paste_position[1] + cropped.size[1] + math.floor(image_size[1] / max_row_size * crop_overscan * 8),
+            )  # Bottom 3
 
-        paste_position = (	paste_position[2] + math.floor(image_size[0] / max_row_size * crop_overscan  * 2), # Left 0
-                            paste_position[1], # Top 1
-                            paste_position[2] + cropped.size[0] + math.floor(image_size[0] / max_row_size * crop_overscan  * 2), # Right 2
-                            paste_position[3] ) # Bottom 3
-        
-        print('paste_position =', paste_position)
+        paste_position = (
+            paste_position[2] + math.floor(image_size[0] / max_row_size * crop_overscan * 2),  # Left 0
+            paste_position[1],  # Top 1
+            paste_position[2]
+            + cropped.size[0]
+            + math.floor(image_size[0] / max_row_size * crop_overscan * 2),  # Right 2
+            paste_position[3],
+        )  # Bottom 3
+
+        print("paste_position =", paste_position)
         final_image.paste(cropped, paste_position)
 
     image_number += 1
@@ -242,20 +259,19 @@ for i, current_image_to_process in enumerate(images_to_process):
 
 resized_dimensions = get_resized_dimensions(image_size, max_destination_resolution)
 if resized_dimensions[0] < image_size[0] and resized_dimensions[1] < image_size[1]:
-    print('Resizing image ...')
+    print("Resizing image ...")
     final_image = final_image.resize((resized_dimensions[0], resized_dimensions[1]))
 
 if save_png:
-    destination = os.path.join(source_folder, output_filename + '_composite.png')
-    print('saving Image : ', destination)
+    destination = os.path.join(source_folder, output_filename + "_composite.png")
+    print("saving Image : ", destination)
     final_image.save(destination)
 if save_jpeg:
-    destination = os.path.join(source_folder, output_filename + '_composite.jpg')
-    print('saving Image : ', destination)
-    jpg = Image.new('RGBA', resized_dimensions, background_color)
+    destination = os.path.join(source_folder, output_filename + "_composite.jpg")
+    print("saving Image : ", destination)
+    jpg = Image.new("RGBA", resized_dimensions, background_color)
     jpg.alpha_composite(final_image)
-    jpg = jpg.convert('RGB')
+    jpg = jpg.convert("RGB")
     jpg.save(destination)
 
-print('Composite Done !!!')
-
+print("Composite Done !!!")
